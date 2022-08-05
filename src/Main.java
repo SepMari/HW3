90fa79fcca88e4d4e495f32f30253bc7a237a983
Maria <sepkina97@mail.ru>
2022-08-05 21:30:47 +0300

    Сделанное ДЗ + сделала задачу повышенного уровня №6

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,12 @@ public class Main {
         //Задание 2
         int clientDeviceYear = 2017;
 
-        if (clientOS == 1) {
-            if (clientDeviceYear > 2015) {
+        if (clientOS == 1 && clientDeviceYear > 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
-            if (clientDeviceYear > 2015) {
+        } else if (clientOS == 0 && clientDeviceYear > 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+        } else {
+                System.out.println("Установите версию приложения по ссылке");
         }
 
         //Задание 3 високосным является каждый четвертый год, но не является каждый сотый.
